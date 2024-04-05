0d560cf3fc959f300ed0593b668b257865df8819
Enrico Varano <enricovarano@gmail.com>
2024-04-05 16:21:14 +0200

    silenced output of splitter code

--- video_splitter/video_splitter.py
+++ video_splitter/video_splitter.py
@@ -13,15 +13,16 @@ output is a video file with the modulated sine wave as its audio track.
 
 """
 
-import numpy as np
+import subprocess
 import os
 import csv
+import numpy as np
 
 #%% General Parameters
 
 video_folder = os.path.join(os.path.dirname(os.getcwd()), "videos")
 csv_folder = os.getcwd()
-output_folder = os.path.join(os.getcwd(), "video_start_end")
+output_folder = os.path.join(os.getcwd(), "audio_start_end")
 
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
@@ -43,13 +44,23 @@ for file in os.listdir(csv_folder):
                 input_video_name = line[0] + ".mp4"
                 output_video_name = line[1] + "_" + line[2] + "_" + line[3] + "_str" + line[4] + "_snt" + str(line[5]).zfill(2) + ".mp4"
                 
-                # start_time = float(line[7]) # audio
-                # end___time = float(line[9]) # audio
+                start_time = float(line[7]) # audio
+                end___time = float(line[9]) # audio
                 
-                start_time = float(line[8]) # video
-                end___time = float(line[10]) # video
+                # start_time = float(line[8]) # video
+                # end___time = float(line[10]) # video
                 
                 start_time = start_time - start_buffer
                 end___time = end___time + end___buffer
                 
-                os.system(f"ffmpeg -y -ss {start_time} -i \"{os.path.join(video_folder, input_video_name)}\" -t {end___time - start_time} -c:v copy -c:a copy \"{os.path.join(output_folder, output_video_name)}\"")
+                duration = str(np.round(end___time - start_time, 2))
+                print(output_video_name, duration)
+                
+                subprocess.run([
+                    "ffmpeg", "-y",
+                    "-ss", str(start_time),
+                    "-i", os.path.join(video_folder, input_video_name),
+                    "-t", duration,  # Calculate duration and convert to string
+                    "-c:v", "copy", "-c:a", "copy",
+                    os.path.join(output_folder, output_video_name)
+                ], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
